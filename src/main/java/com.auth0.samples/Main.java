efb1b09256d11a17889c87e1db7e3a09d6c5cd34
fuzionu <fuzionu@gmail.com>
2020-09-18 21:40:30 +0200

    init build.gradle, .gitignore & Main.java with Tomcat server

---
+++ src/main/java/com.auth0.samples/Main.java
@@ -0,0 +1,13 @@
+package com.auth0.samples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main
+{
+    public static void main(String... args)
+    {
+        SpringApplication.run(Main.class, args);
+    }
+}
